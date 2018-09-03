687c60839851d6a61d8af9863a0e4c4cd67a8b05
Jeff Langevin <jeff.langevin@gmail.com>
2018-09-03 16:01:15 -0400

    Fix paramsSubscription error

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,7 +9,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
-  paramsSubscription
+  paramsSubscription: Subscription;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -16,7 +17,7 @@ export class UserComponent implements OnInit, OnDestroy {
       id: this.route.snapshot.params['id'],
       name:  this.route.snapshot.params['name']
     };
-    this.route.params.subscribe(
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
